ab341ac4d298e54a6b858488947c63647537df8d
hgcosta <hugocosta.devs@gmail.com>
2022-12-23 14:21:42 -0300

    Modificando as rotas utilizando express

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-const http = require('http');
-const port = 3000;
+import app from "./src/app.js";
+const port = process.env.Port  || 3000;
 
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros':'Página Livros',
-    '/autores':'Listagem de autores',
-    '/editora': 'Editora',
-}
 
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-});
-
-
-server.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
 });
